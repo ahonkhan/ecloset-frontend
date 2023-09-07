a07b1f97a32e03afa08f9512d0dcb590b23afd7c
ahonkazi <aohinuzzaman420@gmail.com>
2023-09-07 15:22:13 +0600

    button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import './App.scss';
+import { ButtonGreen, ButtonLgLightBlue, ButtonMdDarkBlue, ButtonYellowOutline, ButtonYellowOutlineIcon } from './public/shared/components/Buttons/Buttons';
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 py-4'>
+      <ButtonLgLightBlue>Load more</ButtonLgLightBlue>
+      <br />
+      <br />
+      <ButtonMdDarkBlue>Join now</ButtonMdDarkBlue>
+      <br /><br />
+      <ButtonGreen>Get Started</ButtonGreen>
+      <br /><br />
+      <div className="bg-slate-800 py-12 px-12">
+        <ButtonYellowOutlineIcon>See how it works </ButtonYellowOutlineIcon>
+
+      </div>
+    </div>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'LightBlue': "#40D7E1",
+        'darkBlue': "#01BEC9",
+        'Green': "#034B47",
+        'yellowLight': "#FEFFD1",
+        'White': "#FFFFFF",
+        'LightGray': "#F6EEE7",
+        'Gray': "#F0F0F0",
+        'DarkGray': "#6B6B6B",
+        'Black': "#000000",
+        'YellowDark': "#F7D334",
+      }
+    },
+  },
+  plugins: [],
+}
