839fb0b20ebc00fbc563d8e35d7fec868eba18f6
ahonkazi <aohinuzzaman420@gmail.com>
2023-09-16 08:48:48 +0600

    added home page component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
   return (
     <>
       <Routes>
+        <Route path='/' element={<h1>This is home page</h1>} />
         <Route path='/tamanna' element={<Tamanna />} />
         <Route path='/ahon' element={<Ahon />} />
       </Routes>
