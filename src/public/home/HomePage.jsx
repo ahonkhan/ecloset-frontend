911d37cd7fd3b4364f731761455774fb1b6b2ed0
ahonkazi <aohinuzzaman420@gmail.com>
2023-09-16 11:48:56 +0600

    home updated

--- src/public/home/HomePage.jsx
+++ src/public/home/HomePage.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import DefaultWrapper from '../shared/components/wrappers/DefaultWrapper'
 import Hero from './components/Hero/Hero'
+import { ProductCard } from '../shared/components/cards/Card'
 const HomePage = () => {
     return (
         <>
             <DefaultWrapper>
                 <Hero />
+                <div className="wrapper site-container mt-8 grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 gap-y-8 gap-4">
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+
+                </div>
             </DefaultWrapper>
         </>
     )
