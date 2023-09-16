e6534cddabf7a556049b0a03c5141c6f116067a7
ahonkazi <aohinuzzaman420@gmail.com>
2023-09-16 11:55:57 +0600

    responsive

--- src/public/home/components/Hero/Hero.jsx
+++ src/public/home/components/Hero/Hero.jsx
@@ -47,45 +47,44 @@ const Hero = () => {
                 <div className="upcoming-events shrink-0 md:w-[280px] lg:w-[400px] xl:w-[500px] py-4 px-4 bg-white rounded-xl h-[400px] md:h-auto lg:h-[400px]">
                     <h2 className='font-WorkSans text-DarkGray font-medium'>Upcoming events</h2>
                     <div className="event-wrapper mt-4 flex flex-col gap-4 h-[330px] overflow-y-auto pr-4" style={{ 'scrollbarWidth': 'none' }}>
-                        <div style={{ 'backgroundImage': "url('" + eventBg + "')" }} className="event-item h-[150px] items-center bg-cover bg-center bg-opacity-30 bg-rose-500  py-4 px-8 rounded-xl grid grid-cols-2">
+
+                        <div style={{ 'backgroundImage': "url('" + eventBg + "')" }} className="event-item h-[150px] items-center bg-cover bg-center bg-opacity-30 bg-rose-500  py-4 px-4 rounded-xl grid grid-cols-2">
                             <div className="" >
                                 <img src={eventLogo} alt="" />
                                 <p className='text-GreenLight font-Poppins font-semibold text-lg'>Event Starts in</p>
                             </div>
-                            <div className="grid grid-cols-2 gap-4">
-                                <div className="bg-GreenLight text-white font-WorkSans font-medium py-1 px-2 rounded-md">
-                                    <p className='p-2 text-xl text-center'>10<span>d</span></p>
+                            <div className="grid grid-cols-2 gap-4 text sm">
+                                <div className="bg-GreenLight text-white font-WorkSans font-medium py-1 lg:px-2 rounded-md">
+                                    <p className='p-2  text-center'>10<span>d</span></p>
                                 </div>
-                                <div className="bg-GreenLight text-white font-WorkSans font-medium py-1 px-2 rounded-md">
-                                    <p className='p-2 text-xl text-center'>1<span>h</span></p>
+                                <div className="bg-GreenLight text-white font-WorkSans font-medium py-1 lg:px-2 rounded-md">
+                                    <p className='p-2  text-center'>1<span>h</span></p>
                                 </div>
-                                <div className="bg-GreenLight text-white font-WorkSans font-medium py-1 px-2 rounded-md">
-                                    <p className='p-2 text-xl text-center'>41<span>m</span></p>
+                                <div className="bg-GreenLight text-white font-WorkSans font-medium py-1 lg:px-2 rounded-md">
+                                    <p className='p-2  text-center'>41<span>m</span></p>
                                 </div>
-                                <div className="bg-GreenLight text-white font-WorkSans font-medium py-1 px-2 rounded-md">
-                                    <p className='p-2 text-xl text-center'>53<span>s</span></p>
+                                <div className="bg-GreenLight text-white font-WorkSans font-medium py-1 lg:px-2 rounded-md">
+                                    <p className='p-2  text-center'>53<span>s</span></p>
                                 </div>
-
                             </div>
                         </div>
-
-                        <div style={{ 'backgroundImage': "url('" + eventBg + "')" }} className="event-item h-[150px] items-center bg-cover bg-center bg-opacity-30 bg-rose-500  py-4 px-8 rounded-xl grid grid-cols-2">
+                        <div style={{ 'backgroundImage': "url('" + eventBg + "')" }} className="event-item h-[150px] items-center bg-cover bg-center bg-opacity-30 bg-rose-500  py-4 px-4 rounded-xl grid grid-cols-2">
                             <div className="" >
                                 <img src={eventLogo} alt="" />
                                 <p className='text-GreenLight font-Poppins font-semibold text-lg'>Event Starts in</p>
                             </div>
-                            <div className="grid grid-cols-2 gap-4">
-                                <div className="bg-GreenLight text-white font-WorkSans font-medium py-1 px-2 rounded-md">
-                                    <p className='p-2 text-xl text-center'>10<span>d</span></p>
+                            <div className="grid grid-cols-2 gap-4 text sm">
+                                <div className="bg-GreenLight text-white font-WorkSans font-medium py-1 lg:px-2 rounded-md">
+                                    <p className='p-2  text-center'>10<span>d</span></p>
                                 </div>
-                                <div className="bg-GreenLight text-white font-WorkSans font-medium py-1 px-2 rounded-md">
-                                    <p className='p-2 text-xl text-center'>1<span>h</span></p>
+                                <div className="bg-GreenLight text-white font-WorkSans font-medium py-1 lg:px-2 rounded-md">
+                                    <p className='p-2  text-center'>1<span>h</span></p>
                                 </div>
-                                <div className="bg-GreenLight text-white font-WorkSans font-medium py-1 px-2 rounded-md">
-                                    <p className='p-2 text-xl text-center'>41<span>m</span></p>
+                                <div className="bg-GreenLight text-white font-WorkSans font-medium py-1 lg:px-2 rounded-md">
+                                    <p className='p-2  text-center'>41<span>m</span></p>
                                 </div>
-                                <div className="bg-GreenLight text-white font-WorkSans font-medium py-1 px-2 rounded-md">
-                                    <p className='p-2 text-xl text-center'>53<span>s</span></p>
+                                <div className="bg-GreenLight text-white font-WorkSans font-medium py-1 lg:px-2 rounded-md">
+                                    <p className='p-2  text-center'>53<span>s</span></p>
                                 </div>
                             </div>
                         </div>
