a88431ba1e9100594f722421537d02b9a31fa083
ahonkazi <aohinuzzaman420@gmail.com>
2023-09-21 12:11:21 +0600

    bug fixed

--- src/public/search/components/wrapper/ProductWrapper.jsx
+++ src/public/search/components/wrapper/ProductWrapper.jsx
@@ -15,7 +15,7 @@ const ProductWrapper = () => {
                     <SearchProducts />
                 </div>
 
-                <div className={`fixed ${context.searchFilterStatus ? '' : 'translate-x-full md:translate-x-0'} duration-300 md:static top-0 right-0 z-[160] md:block w-full xs:w-[300px]  shrink-0`}>
+                <div className={`fixed ${context.searchFilterStatus ? '' : 'translate-x-full md:translate-x-0'} duration-300 md:static top-0 right-0 z-[80] md:z-[40] md:block w-full xs:w-[300px]  shrink-0`}>
                     <div className="relative md:pt-0 z-[160] h-screen md:h-auto overflow-y-auto">
                         <SearchFilter />
                         <button onClick={() => context.setSearchFilterStatus(false)} className={`absolute duration-200 md:hidden rounded-full ${context.searchFilterStatus ? '' : 'translate-x-full'} bg-white p-2 top-0 xs:right-full `}>
@@ -26,7 +26,7 @@ const ProductWrapper = () => {
                         <AiOutlineClose />
                     </button>
                 </div >
-                <div className={`fixed ${context.searchFilterStatus ? 'opacity-100' : 'pointer-events-none opacity-0'} md:hidden duration-300 h-screen w-screen bg-black z-[150] bg-opacity-50 top-0 left-0`}>
+                <div className={`fixed ${context.searchFilterStatus ? 'opacity-100' : 'pointer-events-none opacity-0'} md:hidden duration-300 h-screen z-[70] w-screen bg-black bg-opacity-50 top-0 left-0`}>
                 </div>
             </section >
         </>
