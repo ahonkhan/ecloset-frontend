8939b7b15224a5b2c8fcd9c15b1aee9ed13c5040
ahonkazi <aohinuzzaman420@gmail.com>
2023-10-03 14:10:16 +0600

    checkout button

--- src/public/shared/components/Sidebar/CartSidebar.jsx
+++ src/public/shared/components/Sidebar/CartSidebar.jsx
@@ -4,6 +4,7 @@ import { AiOutlineClose } from 'react-icons/ai';
 import { BiMinus, BiPlus } from 'react-icons/bi';
 import { GetGlobalContext } from '../../../../context/GlobalContext';
 import img from '../../assets/headphone.png';
+import { Link } from 'react-router-dom';
 const CartSidebar = () => {
     const globalcontext = useContext(GetGlobalContext)
     return (
@@ -11,14 +12,14 @@ const CartSidebar = () => {
             <AnimatePresence>
                 {
                     globalcontext.cartSideStatus &&
-                    (<motion.aside transition={{ duration: 0.3 }} initial={{ x: '100%' }} exit={{ x: '100%' }} animate={{ x: 0 }} className='fixed top-0 right-0 w-full xs:w-[350px] md:w-[400px] bg-white h-screen  py-4 z-[160]'>
-                        <div className="">
-                            <div className="cart-header mt-4 px-4 flex justify-end">
+                    (<motion.aside transition={{ duration: 0.3 }} initial={{ x: '100%' }} exit={{ x: '100%' }} animate={{ x: 0 }} className='fixed top-0 right-0 w-full xs:w-[350px] md:w-[400px] bg-white h-screen z-[160]'>
+                        <div className="flex flex-col h-screen ">
+                            <div className="cart-header shrink-0 mt-4 px-4 flex justify-end">
                                 <button onClick={() => { globalcontext.setCartSideStatus(false); document.body.classList.remove('overflow-hidden') }} className='text-xl'>
                                     <AiOutlineClose />
                                 </button>
                             </div>
-                            <ul style={{ 'scrollbarWidth': 'thin' }} className="cart-list font-WorkSans mt-4 py-4 overflow-y-auto h-[90vh]">
+                            <ul style={{ 'scrollbarWidth': 'thin' }} className="cart-list font-WorkSans mt-4  overflow-y-auto h-full">
                                 {
                                     [...new Array(5)].map((item, index) =>
                                         <li className={`flex ${index === 0 ? 'border-t' : ''} px-4 border-b items-center  border-gray-200 py-4 gap-4`}>
@@ -44,6 +45,9 @@ const CartSidebar = () => {
                                     )
                                 }
                             </ul>
+                            <div className="shrink-0">
+                                <Link onClick={() => { globalcontext.setCartSideStatus(false); document.body.classList.remove('overflow-hidden') }} to={'/checkout'} className='w-full block text-center bg-GreenLight text-white py-2.5 font-WorkSans '>Proceed to checkout</Link>
+                            </div>
                         </div>
                     </motion.aside>)
                 }
