c2fcab68ed99fa52c309f3b6d92d9f5d48d0c4ce
ahonkazi <aohinuzzaman420@gmail.com>
2023-10-03 12:21:29 +0600

    cart list

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 
 export const GetGlobalContext = createContext();
 export const GlobalContext = ({ children }) => {
-    const [cartSideStatus, setCartSideStatus] = useState(true);
+    const [cartSideStatus, setCartSideStatus] = useState(false);
     return (
         <GetGlobalContext.Provider value={{ cartSideStatus, setCartSideStatus }}>{children}</GetGlobalContext.Provider>
 
